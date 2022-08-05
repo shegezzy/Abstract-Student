303d551d67c89718136219a974eaac6384d71c77
shegezzy <39660743+shegezzy@users.noreply.github.com>
2022-08-05 12:01:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student mystudent = new Student();
+            mystudent.principal();
+            mystudent.role();
+        }
+    }
+}
---
+++ school.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    // this is an abstract class
+    internal abstract class School
+    {
+        // principal and role does not return any value
+        public abstract void principal();
+        public void role()
+        {
+            Console.WriteLine("Principal is: Head of the school");
+        }
+    }
+    //This class was inherited from the class School
+    class Student : School
+    {
+        public override void principal()
+        {
+            Console.WriteLine("Segun is: first student");
+        }
+    }
+
+}
